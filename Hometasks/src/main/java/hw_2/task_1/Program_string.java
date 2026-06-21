afc0c0d0b73186d7c80e782d15d6285312fd2f1a
7612131-droid <7612131@gmail.com>
2026-06-21 21:01:29 +0300

    hw 2

---
+++ Hometasks/src/main/java/hw_2/task_1/Program_string.java
@@ -0,0 +1,11 @@
+package task_1;
+
+public class Program_string {
+//     Створити змінну string1 = "This line that i want to cut, cause it is too long".
+//    Створити рядок string2 у якому має бути розміщено значення рядка string1,
+//    Обрізане до "This line that i want to cut, cause".
+//    Створити рядок string3 на основі string2 який міститиме значення
+//"This line that don't want to cut, cause it is perfect".
+//    Вивести на консоль кожне повідомлення та його довжину.
+
+}

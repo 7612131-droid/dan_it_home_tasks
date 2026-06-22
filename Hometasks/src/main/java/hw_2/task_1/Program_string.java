fe82dcb38a5ed839892e276ce9f6f72c86a39f66
7612131-droid <7612131@gmail.com>
2026-06-22 19:47:18 +0300

    homework 2 task-1

--- Hometasks/src/main/java/hw_2/task_1/Program_string.java
+++ Hometasks/src/main/java/hw_2/task_1/Program_string.java
@@ -1,11 +1,17 @@
-package task_1;
+package hw_2.task_1;
 
 public class Program_string {
-//     Створити змінну string1 = "This line that i want to cut, cause it is too long".
-//    Створити рядок string2 у якому має бути розміщено значення рядка string1,
-//    Обрізане до "This line that i want to cut, cause".
-//    Створити рядок string3 на основі string2 який міститиме значення
-//"This line that don't want to cut, cause it is perfect".
-//    Вивести на консоль кожне повідомлення та його довжину.
+    public static void main(String[] args) {
 
+        String string1 = "This line that i want to cut, cause it is too long";
+        String string2 = string1.substring(0,35);
+        String string3 = string2 + " " + "it is perfect";
+
+        System.out.println(string2);
+        System.out.println(string2.length());
+        System.out.println(string3);
+        System.out.println(string3.length());
+
+
+    }
 }
